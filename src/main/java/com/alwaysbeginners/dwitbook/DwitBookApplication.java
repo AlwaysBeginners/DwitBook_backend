44fd21707df549f124488be27e473d022ded899e
BecomeWeasel <hsw0194@gmail.com>
2022-04-10 17:12:05 +0900

    first commit

---
+++ src/main/java/com/alwaysbeginners/dwitbook/DwitBookApplication.java
@@ -0,0 +1,13 @@
+package com.alwaysbeginners.dwitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwitBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwitBookApplication.class, args);
+	}
+
+}
